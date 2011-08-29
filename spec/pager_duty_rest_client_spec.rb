38731f44622e529b824d2f5b19419352cb7ae015
Lucas Parry <lucas@envato.com>
2011-08-29 21:02:43 +1000

    Check times are translated to UTC

--- spec/pager_duty_rest_client_spec.rb
+++ spec/pager_duty_rest_client_spec.rb
@@ -1,7 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'time'
 
-describe "PagerDutyRestClient" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe PagerDutyRestClient do
+  subject {PagerDutyRestClient.new('orgname', 'user@orgname.com', 'password')}
+
+  let(:time1) { Time.parse '2011-08-29T12:56+10' }
+  let(:expected_time1) { '2011-08-29T02:56+0' }
+  let(:time2) { Time.parse '2011-08-29T22:56-10' }
+  let(:expected_time2) { '2011-08-30T08:56+0' }
+
+  it "correctly converts the dates to UTC" do
+    subject.should_receive(:authenticated_get).with('/api/v1/schedules/code/entries', {'since' => expected_time1, 'until' => expected_time2})
+    subject.get_schedule_data('code', time1, time2)
   end
 end
