c7010e7224fde630671becb89126f1980682f731
Lucas Parry <lucas@envato.com>
2011-12-14 15:11:44 +1100

    Adding https support
    NFI how to write tests for this :/

--- lib/pager_duty_rest_client.rb
+++ lib/pager_duty_rest_client.rb
@@ -1,10 +1,18 @@
 require 'net/http'
 require 'json'
 require 'pager_duty_rest_client/version'
+begin
+  require 'net/https'
+rescue LoadError
+  puts "net/https was not found on your system, ssl support will not work without it"
+end
 
 class PagerDutyRestClient
 
-  def initialize(org_name, username, password)
+  RootCA = '/etc/ssl/certs'
+
+  def initialize(org_name, username, password, options = {})
+    @options = {:https => false}.merge(options)
     @org_name = org_name
     @username = username
     @password = password
@@ -31,12 +39,25 @@ class PagerDutyRestClient
 
   private
 
+  def use_https?
+    !!@options[:https]
+  end
+
+  def protocol
+    use_https? ? "https" : "http"
+  end
+
   def authenticated_get(path, form_data)
-    url = URI.parse("http://#{@org_name}.pagerduty.com#{path}")
+    url = URI.parse("#{protocol}://#{@org_name}.pagerduty.com#{path}")
+
     req = Net::HTTP::Get.new(url.path)
     req.basic_auth @username, @password
     req.set_form_data(form_data, ';')
-    res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
+
+    http = Net::HTTP.new(url.host, url.port)
+    configure_ssl(http) if use_https?
+    res = http.start {|http| http.request(req) }
+
     case res
     when Net::HTTPSuccess, Net::HTTPRedirection
       JSON.parse(res.body)
@@ -44,6 +65,18 @@ class PagerDutyRestClient
       res.error!
     end
 
+  end
+
+  def configure_ssl(http)
+    http.use_ssl = true
+
+    if File.directory? RootCA
+      http.ca_path = RootCA
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.verify_depth = 5
+    else
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
   end
 
   def pagerduty_time_string(time)
